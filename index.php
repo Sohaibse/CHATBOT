ded347afaa461f8a024f0f62a726afc65caf0e19
Muhammad Sohaib <40931132+Sohaibse@users.noreply.github.com>
2021-11-23 18:10:11 +0500

    revert changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <!-- <script src="js/chatbot.js"></script> -->
+        <script src="js/chatbot.js"></script>
     </head>
     <body>
         <?php
